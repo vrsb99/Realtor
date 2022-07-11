9bb565716f7b393d046a116725ccdef209458a44
Vignesh <vigneshrsb99@gmail.com>
2022-07-11 22:25:13 +0800

    Created Navbar

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'next/link';
+import { Menu, MenuButton, MenuList, MenuItem, IconButton, Flex, Box, Spacer } from '@chakra-ui/react';
+import { FcMenu, FcHome, FcAbout } from 'react-icons/fc'
+import { BsSearch } from 'react-icons/bs';
+import { FiKey } from 'react-icons/fi';
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+
+    );
+}
+
+export default Navbar;
