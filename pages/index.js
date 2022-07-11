d45b11c71bfbfb3fae0e03e659234df2a1ecc879
Vignesh <vigneshrsb99@gmail.com>
2022-07-11 19:09:26 +0800

    2nd commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-
 import Link from 'next/link';
 import Image from 'next/image';
-import {Flex, Box, Text, Button} from '@chakra-ui/core';	// Importing the components from the @chakra-ui/core library
+import { Flex, Box, Text, Button } from '@chakra-ui/react';	// Importing the components from the @chakra-ui/react library
 
+const Banner = ({ purpose, title1, title2, description1, description2, buttonText, linkName, imageUrl }) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
+    <Box p="5">
+      <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
+      <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
+      <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{description1}<br />{description2}</Text>
+      <Button fontSize="xl" bg="blue.300" color="white">
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
+  </Flex>
+)
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-     
+    <div>
+      <h1>Hello World</h1>
+      <Banner 
+      purpose="RENT A HOME"
+      title1="Rental Home for"
+      title2= "Everyone"
+      description1="Explore Apartments, Condos, and Houses for Rent"
+      description2="and more"
+      buttonText="Explore Renting"
+      linkName='/search?purpose=for-rent'
+      imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
+      />
+      <Banner
+       purpose='BUY A HOME'
+       title1= "Find, Buy & Own Your Home"
+       title2='Dream Home'
+       description1=' Explore from Apartments, land, builder floors,'
+       description2=' villas and more'
+       buttonText='Explore Buying'
+       linkName='/search?purpose=for-sale'
+       imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
+      />
     </div>
   )
 }
