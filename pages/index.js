e917fe03b7e648b0bb3d54757097c826d1feac8a
Vignesh <vigneshrsb99@gmail.com>
2022-07-11 18:52:22 +0800

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+
+import Link from 'next/link';
+import Image from 'next/image';
+import {Flex, Box, Text, Button} from '@chakra-ui/core';	// Importing the components from the @chakra-ui/core library
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+     
+    </div>
+  )
+}
